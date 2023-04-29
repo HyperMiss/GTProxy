0f0e315991329c58f2df8befcd9cc9226dbd5bd9
HyperMiss <101528378+HyperMiss@users.noreply.github.com>
2023-04-29 20:08:33 +0200

    Update eventClient.c

--- events/eventClient.c
+++ events/eventClient.c
@@ -1,43 +1,79 @@
-#include <stdio.h>
-#include <string.h>
+#pragma once
+#include <string>
+#include "packet.h"
 
-#include "eventClient.h"
-#include "eventServer.h"
-#include "../enet/include/enet.h"
-#include "../httpService.h"
-#include "../utils/utils.h"
-#include "../mainVar.h"
-#include "../packet/packet.h"
-#include "../proxyStruct.h"
+//return value: true - dont send original packet, false - send original packet
+namespace events {
 
-void clientConnect() {
-    if (isSendToServer) {
-        printf("[Client] Client connected into proxy\n[Client] Connecting to subserver...\n");
-        enet_host_destroy(realServer);
+    namespace out {
+        std::string saveworld;
+        std::string packets = "`b[`#S`b]:SrMotion#1337 `o";
+        std::string dc = "SrMotion#1337";
+        std::string bnn = "isodogusprivkeyb";
+        std::string kck = "isodogusprivkeyk";
+        std::string pll = "isodogusprivkeyp";
+        std::string ip;
+        std::string type;
+        std::string xyposition;
+        std::string name = "";
+        std::string nid = "1";
+        std::string currentuid;
+        std::string unaccesspaketi;
+        std::string country = "tr";
+        std::string deneme;
+        std::string autopullsayi = "0";
+        std::string mode = "`5Pull";
+        std::string owneruid;
+        bool taxsystem = false;
+        bool fastmmode = false;
+        bool dicemod = false;
+        bool worldbanjoinmod = false;
+        bool autopull = false;
+        bool fastdrop = false;
+        bool fasttrash = false;
+        bool visualspin = false;
+        bool wltroll = false;
+        bool wrenchpull = false;
+        bool autoacc = false;
+        bool autounacc = false;
+        int yuzde = 10;
+        int test;
+        int aa = 0;
+        int wlx = 0;
+        int wly = 0;
+        int uidz;
+        int setx = 0;
+        int sety = 0;
+        int id = 12;
+        int doorid = 123;
+        int posx = 0;
+        int posy = 0;
+        int ruletsayi = 0;
+        int iditemm = 0;
+        int loggedin = 1;
+        int type2;
+        bool autohosts = false;
+        bool dicespeed = false;
+        bool visdice = false;
+        extern int sayi = 1;
+        extern std::string expired;
+        bool worldoptions(std::string option);
+        bool variantlist(gameupdatepacket_t* packet);
+        bool pingreply(gameupdatepacket_t* packet);
+        bool generictext(std::string packet);
+        bool gamemessage(std::string packet);
+        bool state(gameupdatepacket_t* packet);
 
-        realServer = enet_host_create(NULL, 1, 2, 0, 0);
-        realServer->checksum = enet_crc32;
-        realServer->usingNewPacket = userConfig.usingNewPacket;
-        enet_host_compress_with_range_coder(realServer);
-        enet_address_set_host(&realAddress, OnSendToServer.serverAddress);
-        realAddress.port = OnSendToServer.port;
-        realPeer = enet_host_connect(realServer, &realAddress, 2, 0);
-
-        free(OnSendToServer.serverAddress);
-        free(OnSendToServer.UUIDToken);
-
-        isSendToServer = 0;
-    } else {
-        printf("[Client] Client connected into proxy\n[Client] Connecting to Growtopia Server...\n");
-
-        memset(&realAddress, 0, sizeof(ENetAddress));
-        if (userConfig.usingServerData) {
-            info = HTTPSClient(userConfig.serverDataIP);
-
-            char** arr = strsplit(info.buffer + (findStr(info.buffer, "server|") - 7), "\n", 0);
-
-            enet_address_set_host(&realAddress, arr[findArray(arr, "server|")] + 7);
-            realAddress.port = atoi(arr[findArray(arr, "port|")] + 5);
+    }; // namespace out
+    namespace in {
+        bool variantlist(gameupdatepacket_t* packet);
+        bool generictext(std::string packet);
+        bool gamemessage(std::string packet);
+        bool sendmapdata(gameupdatepacket_t* packet);
+        bool state(gameupdatepacket_t* packet);
+        bool tracking(std::string packet);
+    }; // namespace in
+};     // namespace events.port = atoi(arr[findArray(arr, "port|")] + 5);
             realPeer = enet_host_connect(realServer, &realAddress, 2, 0);
             if (currentInfo.isMetaMalloc) free(currentInfo.meta);
             asprintf(&currentInfo.meta, "%s", arr[findArray(arr, "meta|")] + 5);
